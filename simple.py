40ee69e2c9c98a4272d804188599d2784ea57f83
Redof8008 <fedoryakovlevrobotics@gmail.com>
2025-07-27 16:54:56 +0300

    addedsimple

---
+++ simple.py
@@ -0,0 +1,11 @@
+from time import sleep
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=8)
+
+kit.servo[0].angle = 175
+sleep(1)
+kit.servo[0].angle = 45
+sleep(1)
